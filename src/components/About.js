55b50e34db1defafe82ed68efd9c3e8b18a42426
douglasfrancis <dougiefrancis@gmail.com>
2023-03-14 21:18:55 +0000

    remove box shadow on logo

--- src/components/About.js
+++ src/components/About.js
@@ -10,18 +10,18 @@ export default function About() {
 
         <div className='txt'>
           <h1 style={{color:'#3b4a69'}}>ABOUT</h1>
-          <p >Hi, my name is Laura and I’m the new founder and business owner of Up Hill ‘n’
-            Down Dale Home Boarding, Doggie Day Care, Dog Walking and Dog Taxi. After 7
+          <p >My name is Laura and I’m the owner of Up Hill ‘n’
+            Down Dale Home Boarding, Doggie Day Care, Dog Walking and Dog Taxi. <br/>After 7
             years in Policing on the Rural Wildlife Team, I have decided to pursue my dreams of
             working with dogs. Dogs have always been a big part in my life, I can’t imagine my
-            life without a dog!
-            I live in Buglawton, Congleton, Cheshire with my Husband, two boys and my doggie
+            life without a dog!<br/>
+            I live in Congleton with my Husband, two boys and my doggie
             Beth, a Patterdale Terrier she’s almost 15 years old! I’ve previously owned Border
-            Terriers, Lurchers, greyhounds and Spaniels. Dogs will always be my passion!
+            Terriers, Lurchers, greyhounds and Spaniels. Dogs will always be my passion!<br/>
             I understand how worrying and stressful it can be for dog owners leaving their dogs
             with someone whilst they are away on holiday. I can assure you; they will feel at
             home and receive the same love from me as they do at home, with lots of walks,
-            playtime, treats and cuddles!
+            playtime, treats and cuddles!<br/>
             We are fully Insured, CRB/DBS checked, and I also have Dog First Aid and
             Emergency Canine Care qualification accredited by CPD standards office.
           </p>
